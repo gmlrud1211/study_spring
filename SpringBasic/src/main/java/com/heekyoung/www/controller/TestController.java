c5df2f1273ac45e5996a5dd11b413b03ac677c4c
gmlrud1211 <gmlrud1211@naver.com>
2019-04-03 17:22:31 +0900

    testcontroller추가

---
+++ SpringBasic/src/main/java/com/heekyoung/www/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.heekyoung.www.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class TestController {
+
+	@RequestMapping(value="/test")
+	public void test() {
+		
+	}
+}
