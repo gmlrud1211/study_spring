0d38249abe7046deb4e72d76e802be78796f7276
gmlrud1211 <gmlrud1211@naver.com>
2019-04-04 20:02:23 +0900

    전체조회, dname으로 조회 추가

--- MyBatis/src/dao/DeptDao.java
+++ MyBatis/src/dao/DeptDao.java
@@ -1,9 +1,18 @@
 package dao;
 
+import java.util.List;
+
 import dto.Dept;
 
 public interface DeptDao {
+	
 	//deptno로 조회하기
 	public Dept selectByDeptno(int deptno);
+
+	//전체조회하기
+	public List<Dept> selectAll();
+
+	//dname으로 조회하기
+	public Dept selectByDname(String str);
 
 }
--- MyBatis/src/main/MyBatisEx.java
+++ MyBatis/src/main/MyBatisEx.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
 import dao.DeptDao;
@@ -21,9 +23,23 @@ public class MyBatisEx {
 		DeptDao deptDao = sqlSession.getMapper(DeptDao.class); //네임스페이스 걸었던걸 찾아옴
 		
 		//마이바티스 호출
-		Dept result = deptDao.selectByDeptno(20);
+		Dept result = deptDao.selectByDeptno(10);
 		
 		System.out.println(result);
+		System.out.println("---------------");
+		
+		List<Dept> list = deptDao.selectAll();
+		for(Dept d : list) {
+			System.out.println(d);
+		}
+		
+		System.out.println("---------------");
+		
+		String str = "SALES";
+		Dept result2 = deptDao.selectByDname(str);		
+		
+		System.out.println(result2);
+		System.out.println("---------------");
 	}
 
 }
