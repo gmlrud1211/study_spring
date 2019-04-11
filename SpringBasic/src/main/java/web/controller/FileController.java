d9aeaff9726250e977e9564b191bca3d9e9120be
gmlrud1211 <gmlrud1211@naver.com>
2019-04-11 20:22:10 +0900

    제목 파라미터로 추가

--- SpringBasic/src/main/java/web/controller/FileController.java
+++ SpringBasic/src/main/java/web/controller/FileController.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
@@ -36,7 +37,7 @@ public class FileController {
 	}
 	
 	@RequestMapping(value="/file/fileup", method=RequestMethod.POST)
-	public String fileupload(MultipartFile file, Upload upload) {
+	public String fileupload(MultipartFile file, Upload upload,String title) {
 		logger.info("파일업로드!");
 		
 		logger.info(file.getName());
